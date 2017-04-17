ac5a50c59f9920ac235c4ac3f370ef66acdd4de7
Edmund Wippler <ed.wippler@gmail.com>
2017-04-17 10:20:55 -0500

    functions working on home and auto sections of balance sheet liabilities

--- server/public/scripts/controllers/balancesheet.controller.js
+++ server/public/scripts/controllers/balancesheet.controller.js
@@ -11,16 +11,97 @@ app.controller('BalanceSheetController', function(ClientFactory) {
     self.user.heloc = true;
     //
     self.housingSum = 0;
-    addHousing();
+    self.transportationSum = 0;
+    self.loanSum = 0;
+    self.unpaidBillSum = 0;
+    self.collectionSum = 0;
 
-    function addHousing() {
-      self.housingSum += Number (self.user.mortgageBalance);
-      if (self.user.heloc == true) {
-          self.housingSum += Number (self.user.helocBalance);
-      }else if (self.user.otherRealEstateValue != null) {
-          self.housingSum += Number (self.user.otherRealEstateBalance);
+    self.totalLiabilities = self.housingSum + self.transportationSum + self.loanSum + self.unpaidBillSum + self.collectionSum;
+
+    // housing section
+    self.addHousing = function(subSection) {
+      if (subSection == null) {
+        subSection = 0;
       }
+      self.housingSum += Number (subSection);
       return self.housingSum;
+    }
+
+    self.updateHouse = function(inputField){
+      if (inputField == null) {
+        inputField = 0;
+      }
+      self.housingSum -=  inputField
+      return self.housingSum;
+    }
+
+    //transport section
+    self.addAuto = function(subSection) {
+      if (subSection == null) {
+        subSection = 0;
+      }
+      self.transportationSum += Number (subSection);
+      return self.transportationSum;
+    }
+
+    self.updateAuto = function(inputField){
+      if (inputField == null) {
+        inputField = 0;
+      }
+      self.transportationSum -=  inputField
+      return self.transportationSum;
+    }
+
+    // loan section
+    self.addLoans = function(subSection) {
+      if (subSection == null) {
+        subSection = 0;
+      }
+      self.loanSum += Number (subSection);
+      return self.loanSum;
+    }
+
+    self.updateLoans = function(inputField){
+      if (inputField == null) {
+        inputField = 0;
+      }
+
+      self.loanSum -=  inputField
+      return self.loanSum;
+    }
+
+    // bills section
+    self.addBills = function(subSection) {
+      if (subSection == null) {
+        subSection = 0;
+      }
+      self.unpaidBillSum += Number (subSection);
+      return self.unpaidBillSum;
+    }
+
+    self.updateBills = function(inputField){
+      if (inputField == null) {
+        inputField = 0;
+      }
+      self.unpaidBillSum -=  inputField
+      return self.unpaidBillSum;
+    }
+
+    //collection section
+    self.addCollection = function(subSection) {
+      if (subSection == null) {
+        subSection = 0;
+      }
+      self.collectionSum += Number (subSection);
+      return self.collectionSum;
+    }
+
+    self.updateCollection = function(inputField){
+      if (inputField == null) {
+        inputField = 0;
+      }
+      self.collectionSum -=  inputField
+      return self.collectionSum;
     }
 
 
