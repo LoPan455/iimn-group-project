3fe9e4d3322a9c054f60845f88f10ec2ea9036f1
Lopan455 <tjohander@gmail.com>
2017-04-20 10:15:11 -0500

    First Commit

--- server/public/scripts/controllers/welcome.controller.js
+++ server/public/scripts/controllers/welcome.controller.js
@@ -7,23 +7,14 @@ app.controller('WelcomeController', function($location,ClientFactory,LoginFactor
   self.testMessage = ClientFactory.testMessage;
   self.clientTester = ClientFactory.clientTester;
 
-self.submit = function(dude) {
-console.log("dude = ", dude);
-ClientFactory.newClient(dude);
-};
-
-
-
-  self.client = ClientFactory.client;
-
-
-
+  self.saveNewClient = function(client) {
+  console.log('saveNewClient() called - object is: ', client);
+  ClientFactory.newClient(client);
+  };
 
   self.logOut = function(){
     console.log('logout button clicked');
     LoginFactory.logOut();
-
-
   };
 
 
