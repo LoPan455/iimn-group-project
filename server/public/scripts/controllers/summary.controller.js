eb931213d9dcd31c2799a0a6c54c03ec895081b8
Paul Dunkirk <pauldunkirk@gmail.com>
2017-04-21 15:52:22 -0500

    styles

--- server/public/scripts/controllers/summary.controller.js
+++ server/public/scripts/controllers/summary.controller.js
@@ -16,7 +16,7 @@ app.controller('SummaryController', function(ClientFactory) {
   self.suggestedTransportation = self.client.totalMonthlyIncome * 0.1;
   self.suggestedHealth = self.client.totalMonthlyIncome * 0.05;
   self.suggestedSavings = self.client.totalMonthlyIncome * 0.1;
-  self.suggestedMisc = self.client.totalMonthlyIncome * 0.15; 
+  self.suggestedMisc = self.client.totalMonthlyIncome * 0.15;
 
 
   self.benchmarkData = [35, 5, 10, 10, 5, 10, 15];
@@ -29,7 +29,7 @@ app.controller('SummaryController', function(ClientFactory) {
       };
 
 
-
+// client pie-chart populated
   self.clientData = [self.client.monthlyTotalHousingExpenses, self.client.monthlyTotalUtilities, self.client.monthlyTotalFoodExpenses, self.client.monthlyTotalTransportationExpenses, self.client.monthlyTotalHealthRelatedExpenses, self.client.monthlySavingsTarget, self.totalLifeExpenses ];
   self.clientLabels = ["Housing", "Utilities", "Food", "Transportation", "Health", "Savings", "Miscellaneous"];
   self.clientOptions = {
