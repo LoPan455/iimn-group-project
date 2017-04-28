6d144cc19eb3a342ae849f59a9c1c2e3d778779c
Lopan455 <tjohander@gmail.com>
2017-04-28 11:05:21 -0500

    Prep for Deploy

--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -1,12 +1,22 @@
 // This willl process the encyrpted token attached to any inbound requests and check the 'users' db collection to confirm that the requestor exists in the db.
 
 var admin = require("firebase-admin");
-var serviceAccount = require("../firebase-service-account.json");
+// var serviceAccount = require("../firebase-service-account.json");
 var UserMon = require('../models/user');
 
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    "type": process.env.FIREBASE_SERVICE_ACCOUNT_TYPE,
+    "project_id": process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+    "token_uri": process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL}),
   databaseURL: "https://fhat-iimn-e55ca.firebaseio.com"
 });
 
